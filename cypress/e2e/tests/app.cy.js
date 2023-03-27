994b350f13d14e4741e8f2c368b8e5f684289d9b
yisus37 <116197767+yisus37@users.noreply.github.com>
2023-03-26 22:40:42 -0600

    .

--- cypress/e2e/tests/app.cy.js
+++ cypress/e2e/tests/app.cy.js
@@ -9,7 +9,7 @@ describe('App.cy.js', () => {
     cy.get('#inp1').type("jose")
     cy.get('#inp2').type("hernandez")
     cy.get('#btn').click()
-    cy.get('#nom').contains("tu  nombre es jose hernandez")
+    cy.get('#nom').contains("tu nombre es jose hernandez")
     
   })
 })
